32e5abdd592db99d1fd7c17f002e85226b6e1b05
Ngoc Tuan <tuansaker1412@gmail.com>
2024-04-23 16:51:43 +0700

    complete style adminlte

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
 end
